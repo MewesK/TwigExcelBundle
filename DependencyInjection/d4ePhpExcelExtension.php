edf747940535f7c4f1416d18eb3908069ae47677
Mewes Kochheim <kochheim@develop4edu.de>
2014-01-27 00:10:32 +0100

    commented out unused variable

--- DependencyInjection/d4ePhpExcelExtension.php
+++ DependencyInjection/d4ePhpExcelExtension.php
@@ -19,7 +19,7 @@ class d4ePhpExcelExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
+        //$configuration = new Configuration();
 
         //$config = $this->processConfiguration($configuration, $configs);
 
