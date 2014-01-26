5bbd1a5722d8edd8d774f9fb62976e0050ade6e8
Mewes Kochheim <kochheim@develop4edu.de>
2014-01-27 00:28:44 +0100

    added all supproted formats

--- EventListener/RequestListener.php
+++ EventListener/RequestListener.php
@@ -8,6 +8,9 @@ class RequestListener
 {
     public function onKernelRequest(GetResponseEvent $event)
     {
-        $event->getRequest()->setFormat('xls', 'application/vnd.ms-excel');
+        $event->getRequest()->setFormat('csv', 'text/csv'); // CSV
+        $event->getRequest()->setFormat('xls', 'application/vnd.ms-excel'); // Excel5
+        $event->getRequest()->setFormat('xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'); // Excel2007
+        $event->getRequest()->setFormat('pdf', ' application/pdf'); // PDF
     }
 }
