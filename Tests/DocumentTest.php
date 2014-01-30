80197ff3a39ef3ebcddcef52fc001f4d109ae30f
Mewes Kochheim <kochheim@develop4edu.de>
2014-01-30 16:02:33 +0100

    more test stuff

--- Tests/DocumentTest.php
+++ Tests/DocumentTest.php
@@ -6,46 +6,45 @@ use MewesK\PhpExcelTwigExtensionBundle\Twig\PhpExcelExtension;
 
 class DocumentTest extends \PHPUnit_Framework_TestCase
 {
+    private $environment;
+
+    public function setUp() {
+        $this->environment = new \Twig_Environment(
+            new \Twig_Loader_Array([
+                'documentSimple' => file_get_contents(__DIR__.'/Resources/views/documentSimple.xls.twig')
+            ]),
+            ['strict_variables' => true]
+        );
+        $this->environment->addExtension(new PhpExcelExtension());
+        $this->environment->setCache(__DIR__.'/../tmp/');
+    }
+
     public function testDocumentSimple()
     {
-        $environment = $this->prepareEnvironment([
-            'documentSimple' => $this->loadTemplate('documentSimple.xls.twig')
-        ]);
-        $template = $environment->loadTemplate('documentSimple');
-
         try {
-            $source = $template->render(['app' => ['request' => ['requestFormat' => 'xls']]]);
-
-            //$this->saveResult('documentSimple.xls', $source);
-
-            $this->assertEquals(
-                md5($source),
-                md5($this->loadReference('documentSimple.xls'))
+            // generate source from template
+            $source = $this->environment->loadTemplate('documentSimple')->render(
+                ['app' => ['request' => ['requestFormat' => 'xls']]]
             );
+
+            // save source
+            file_put_contents(__DIR__.'/Resources/results/documentSimple.xls',$source);
+
+            // load source
+            $reader = new \PHPExcel_Reader_Excel5();
+            $phpExcel = $reader->load(__DIR__.'/Resources/results/documentSimple.xls');
+
+            // test
+            $sheet = $phpExcel->getSheetByName('Test');
+            $this->assertNotNull($sheet);
+            $this->assertEquals($sheet->getCell('A1')->getValue(), 'Foo');
+            $this->assertEquals($sheet->getCell('B1')->getValue(), 'Bar');
+            $this->assertEquals($sheet->getCell('A2')->getValue(), 'Hello');
+            $this->assertEquals($sheet->getCell('B2')->getValue(), 'World');
         } catch (\Twig_Error_Runtime $e) {
+            var_dump($e);
             $this->fail($e->getMessage());
         }
-    }
-
-    public function loadTemplate($name) {
-        return file_get_contents(__DIR__.'/Resources/views/'.$name);
-    }
-
-    public function loadReference($name) {
-        return file_get_contents(__DIR__.'/Resources/references/'.$name);
-    }
-
-    public function saveResult($name, $source) {
-        file_put_contents(__DIR__.'/Resources/references/'.$name, $source);
-    }
-
-    public function prepareEnvironment($templates) {
-        $environment = new \Twig_Environment(new \Twig_Loader_Array($templates), ['strict_variables' => true]);
-        $environment->addExtension(new PhpExcelExtension());
-
-        //$environment->setCache(__DIR__.'/../tmp/');
-
-        return $environment;
     }
 }
  
