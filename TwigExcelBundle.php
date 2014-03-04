38e4e7fdc96fd27a586ad7254580787a95ac3f28
Mewes Kochheim <mewes@kochheim.de>
2014-03-04 23:26:27 +0100

    added code to load config

--- TwigExcelBundle.php
+++ TwigExcelBundle.php
@@ -2,8 +2,16 @@
 
 namespace MewesK\TwigExcelBundle;
 
+use MewesK\TwigExcelBundle\DependencyInjection\Configuration;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TwigExcelBundle extends Bundle
 {
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+
+        $config = $this->processConfiguration($configuration, $configs);
+    }
 }
