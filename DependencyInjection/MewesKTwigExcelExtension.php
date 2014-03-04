a3e99f583e50b6d62af788eaaa78738231f93528
Mewes Kochheim <mewes@kochheim.de>
2014-03-04 23:44:51 +0100

    renamed DI class

--- DependencyInjection/MewesKTwigExcelExtension.php
+++ DependencyInjection/MewesKTwigExcelExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class MewesKTwigExcelBundle extends Extension
+class MewesKTwigExcelExtension extends Extension
 {
     /**
      * {@inheritDoc}
