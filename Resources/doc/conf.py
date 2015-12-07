d48b55bca6b847e738d49da4f95e6cbe069af72f
Mewes Kochheim <mewes@kochheim.de>
2015-12-07 01:51:27 +0100

    Modularized and reformatted documentation

--- Resources/doc/conf.py
+++ Resources/doc/conf.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 source_suffix = '.rst'
 master_doc = 'index'
+project = 'TwigExcelBundle'
+copyright = u'2014-2015, Mewes Kochheim'
+version = ''
+release = ''
+html_theme = 'default'
